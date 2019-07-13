55e18c145d81810e26d0fa6e334a38fe0e2d69dd
elijahmm17 <elijahmm17@gmail.com>
2019-07-13 20:30:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ class App < Sinatra::Base
   post '/newteam' do 
     @name = params[:name]
     @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end 
 end
